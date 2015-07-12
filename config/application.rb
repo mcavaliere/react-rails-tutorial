33e4c0f5a9106077ec79e327ade423b4ae018f22
Mike Cavaliere <mike@cavaliere.org>
2015-07-12 17:42:20 -0400

    Enabling react version with addons

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RailsReactTutorial
 
     # For not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use react JS version with addons included.
+    config.react.addons = true 
   end
 end
