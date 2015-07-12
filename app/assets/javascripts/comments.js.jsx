6afc7877a8e279d75f71849fe6e4e03837c434d5
Mike Cavaliere <mike@cavaliere.org>
2015-07-12 17:55:34 -0400

    Animated height transition form form

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -114,25 +114,33 @@ var CommentForm = React.createClass({
     this.refs.text.getDOMNode().value = '';
     return;
   },
+  getContainerClasses: function() {
+    return React.addons.classSet({
+      "comment-form-container": true,
+      "visible": this.state.visible
+    });
+  },
   getDisplayProperty: function() {
     return (this.state.visible === true ? "block" : "none");
   },
   render: function() {
     return (
-      <div className="panel panel-default" style={{ display: this.getDisplayProperty() }}>
-        <div className="panel-heading">Add a Note</div>
-        <div className="panel-body">
-          <form className="commentForm " onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label className="control-label" forName="authorInput">Author</label>
-              <input type="text" id="authorInput" className="form-control" placeholder="Your name" ref="author" />
-            </div>
-            <div className="form-group">
-              <label className="control-label" forName="commentInput">Comment</label>
-              <textarea className="form-control" id="commentInput" rows="3" placeholder="Say something..." ref="text" />
-            </div>
-            <input type="submit" className="btn btn-primary" value="Post" />
-          </form>
+      <div className={this.getContainerClasses()}>
+        <div className="panel panel-default">
+          <div className="panel-heading">Add a Note</div>
+          <div className="panel-body">
+            <form className="commentForm " onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label className="control-label" forName="authorInput">Author</label>
+                <input type="text" id="authorInput" className="form-control" placeholder="Your name" ref="author" />
+              </div>
+              <div className="form-group">
+                <label className="control-label" forName="commentInput">Comment</label>
+                <textarea className="form-control" id="commentInput" rows="3" placeholder="Say something..." ref="text" />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Post" />
+            </form>
+          </div>
         </div>
       </div>
     );
