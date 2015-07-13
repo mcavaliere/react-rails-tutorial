c6842f8b088648ac742e03bc3b10c2003aea7661
Mike Cavaliere <mike@cavaliere.org>
2015-07-13 10:40:58 -0400

    Hiding "add comment" button when showing form

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -96,6 +96,8 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
   close: function() {
     this.setState({ visible: false });
+
+    $(document).trigger("hide-add-form");
   },
   componentDidMount: function() {
     $(document).on("show-add-form", function(e) {
@@ -153,12 +155,35 @@ var CommentForm = React.createClass({
 });
 
 var CommentButton = React.createClass({
+  getClasses: function() {
+    var classes = {
+      "comment-button": true,
+      "btn": true,
+      "btn-primary": true,
+      "visible": this.state.visible
+    };
+
+    return React.addons.classSet( classes );
+  },
+  getInitialState: function() {
+    return ({ visible: true });
+  },
+  componentDidMount: function() {
+    // Hide the button when we're showing the form
+    $(document).on("show-add-form", function() {
+      this.setState({ visible: false });
+    }.bind(this));
+
+    $(document).on("hide-add-form", function() {
+      this.setState({ visible: true });
+    }.bind(this));
+  },
   handleClick: function(e) {
     $(document).trigger("show-add-form");
   },
   render: function() {
     return (
-      <button className="btn btn-primary" onClick={this.handleClick}>Add a Comment</button>
+      <button className={this.getClasses()} onClick={this.handleClick}>Add a Comment</button>
     );
   }
 });
